21a269d3094e0ce650a578c901fe53be0473ed0b
VeljkoP23 <veljkopet23@gmail.com>
2026-02-23 21:39:41 +0100

    sitne popravke za deploy

--- laravel/docker/entrypoint.sh
+++ laravel/docker/entrypoint.sh
@@ -1,21 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/sh
 set -e
 
-cd /var/www
-
-if [ ! -f ".env" ] && [ -f ".env.example" ]; then
-  cp .env.example .env
-fi
-
-if [ -f "composer.json" ]; then
-  composer install --no-interaction
-fi
-
-if [ -f "artisan" ]; then
-  php artisan key:generate --force || true
-  php artisan config:clear || true
-  php artisan route:clear || true
-  php artisan cache:clear || true
-fi
+ 
+mkdir -p /var/www/storage /var/www/bootstrap/cache
+chmod -R 775 /var/www/storage /var/www/bootstrap/cache || true
 
 exec "$@"
